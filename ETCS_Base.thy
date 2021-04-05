8d8189604546275b9848f30f0904e1cca3fd6feb
James Baxter <jebaxter@hotmail.co.uk>
2021-04-05 15:19:15 +0100

    Ensure function composition axioms require consistent types

--- ETCS_Base.thy
+++ ETCS_Base.thy
@@ -16,7 +16,7 @@ axiomatization
 where
   domain_comp: "domain g = codomain f \<Longrightarrow> domain (g \<circ>\<^sub>c f) = domain f" and
   codomain_comp: "domain g = codomain f \<Longrightarrow> codomain (g \<circ>\<^sub>c f) = codomain g" and
-  comp_associative: "h \<circ>\<^sub>c (g \<circ>\<^sub>c f) = (h \<circ>\<^sub>c g) \<circ>\<^sub>c f" and
+  comp_associative: "domain h = codomain g \<Longrightarrow> domain g = codomain f \<Longrightarrow> h \<circ>\<^sub>c (g \<circ>\<^sub>c f) = (h \<circ>\<^sub>c g) \<circ>\<^sub>c f" and
   id_domain: "domain (id X) = X" and
   id_codomain: "codomain (id X) = X" and
   id_right_unit: "f \<circ>\<^sub>c id (domain f) = f" and
