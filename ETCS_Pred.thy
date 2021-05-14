817c39b567b5918414d868730ef0bd9f31bfa7dd
James Baxter <jebaxter@hotmail.co.uk>
2021-05-14 21:28:43 +0100

    Add NOR and OR logic operators

--- ETCS_Pred.thy
+++ ETCS_Pred.thy
@@ -116,27 +116,95 @@ proof (rule ccontr)
 qed
 
 
+subsection \<open>NOR\<close>
+
+definition NOR :: "cfunc" where
+  "NOR = (THE \<chi>. is_pullback  one one (\<Omega>\<times>\<^sub>c\<Omega>) \<Omega> (\<beta>\<^bsub>one\<^esub>) \<t> \<langle>\<f>, \<f>\<rangle> \<chi>)"
+
+lemma NOR_is_pullback:
+  "is_pullback  one one (\<Omega>\<times>\<^sub>c\<Omega>) \<Omega> (\<beta>\<^bsub>one\<^esub>) \<t> \<langle>\<f>, \<f>\<rangle> NOR"
+  unfolding NOR_def
+  using characteristic_function_exists element_monomorphism
+  by (typecheck_cfuncs, rule_tac the1I2, simp_all)
+
+lemma NOR_type[type_rule]:
+  "NOR : (\<Omega>\<times>\<^sub>c\<Omega>) \<rightarrow> \<Omega>"
+  using NOR_is_pullback unfolding is_pullback_def square_commutes_def by auto
+
+lemma NOR_false_false_is_true:
+  "NOR \<circ>\<^sub>c \<langle>\<f>,\<f>\<rangle> = \<t>"
+  using NOR_is_pullback unfolding is_pullback_def square_commutes_def
+  by (auto, metis cfunc_type_def id_right_unit id_type one_unique_element)
+
+lemma NOR_left_true_is_false:
+  assumes "p \<in>\<^sub>c \<Omega>"
+  shows "NOR \<circ>\<^sub>c \<langle>\<t>,p\<rangle> = \<f>"
+proof (rule ccontr)
+  assume "NOR \<circ>\<^sub>c \<langle>\<t>,p\<rangle> \<noteq> \<f>"
+  then have "NOR \<circ>\<^sub>c \<langle>\<t>,p\<rangle> = \<t>"
+    using assms true_false_only_truth_values by (typecheck_cfuncs, blast)
+  then have "NOR \<circ>\<^sub>c \<langle>\<t>,p\<rangle> = \<t> \<circ>\<^sub>c id one"
+    using id_right_unit2 true_func_type by auto
+  then obtain j where j_type: "j \<in>\<^sub>c one" and j_id: "\<beta>\<^bsub>one\<^esub> \<circ>\<^sub>c j = id one" and ff_j_eq_tp: "\<langle>\<f>,\<f>\<rangle> \<circ>\<^sub>c j = \<langle>\<t>,p\<rangle>"
+    using NOR_is_pullback assms unfolding is_pullback_def by (typecheck_cfuncs, metis)
+  then have "j = id one"
+    using id_type one_unique_element by blast
+  then have "\<langle>\<f>,\<f>\<rangle> = \<langle>\<t>,p\<rangle>"
+    using cfunc_prod_comp false_func_type ff_j_eq_tp id_right_unit2 j_type by auto
+  then have "\<f> = \<t>"
+    using assms cart_prod_eq2 false_func_type true_func_type by auto
+  then show False
+    using true_false_distinct by auto
+qed
+
+lemma NOR_right_true_is_false:
+  assumes "p \<in>\<^sub>c \<Omega>"
+  shows "NOR \<circ>\<^sub>c \<langle>p,\<t>\<rangle> = \<f>"
+proof (rule ccontr)
+  assume "NOR \<circ>\<^sub>c \<langle>p,\<t>\<rangle> \<noteq> \<f>"
+  then have "NOR \<circ>\<^sub>c \<langle>p,\<t>\<rangle> = \<t>"
+    using assms true_false_only_truth_values by (typecheck_cfuncs, blast)
+  then have "NOR \<circ>\<^sub>c \<langle>p,\<t>\<rangle> = \<t> \<circ>\<^sub>c id one"
+    using id_right_unit2 true_func_type by auto
+  then obtain j where j_type: "j \<in>\<^sub>c one" and j_id: "\<beta>\<^bsub>one\<^esub> \<circ>\<^sub>c j = id one" and ff_j_eq_tp: "\<langle>\<f>,\<f>\<rangle> \<circ>\<^sub>c j = \<langle>p,\<t>\<rangle>"
+    using NOR_is_pullback assms unfolding is_pullback_def by (typecheck_cfuncs, metis)
+  then have "j = id one"
+    using id_type one_unique_element by blast
+  then have "\<langle>\<f>,\<f>\<rangle> = \<langle>p,\<t>\<rangle>"
+    using cfunc_prod_comp false_func_type ff_j_eq_tp id_right_unit2 j_type by auto
+  then have "\<f> = \<t>"
+    using assms cart_prod_eq2 false_func_type true_func_type by auto
+  then show False
+    using true_false_distinct by auto
+qed
+
 subsection \<open>OR\<close>
 
-lemma OR_monomorphism_map_type:
-"\<langle>id \<Omega>,\<t> \<circ>\<^sub>c \<beta>\<^bsub>\<Omega>\<^esub>\<rangle>\<amalg>\<langle>\<t> \<circ>\<^sub>c \<beta>\<^bsub>\<Omega>\<^esub>,id \<Omega>\<rangle>: \<Omega>\<Coprod>\<Omega> \<rightarrow> \<Omega>\<times>\<^sub>c\<Omega>"
-  by (meson cfunc_coprod_type cfunc_prod_type comp_type id_type terminal_func_type true_func_type)
-
-
 definition OR :: "cfunc" where
-  "OR = (THE \<chi>. is_pullback  (\<Omega>\<Coprod>\<Omega>) one (\<Omega>\<times>\<^sub>c\<Omega>) \<Omega> (\<beta>\<^bsub>\<Omega>\<Coprod>\<Omega>\<^esub>) \<t> (\<langle>id \<Omega>,\<t> \<circ>\<^sub>c \<beta>\<^bsub>\<Omega>\<^esub>\<rangle>\<amalg>\<langle>\<t> \<circ>\<^sub>c \<beta>\<^bsub>\<Omega>\<^esub>,id \<Omega>\<rangle>) \<chi>)"
-  
-
-
-lemma OR_is_pullback:
-  "is_pullback (\<Omega>\<Coprod>\<Omega>) one (\<Omega>\<times>\<^sub>c\<Omega>) \<Omega> (\<beta>\<^bsub>\<Omega>\<Coprod>\<Omega>\<^esub>) \<t> (\<langle>id \<Omega>,\<t> \<circ>\<^sub>c \<beta>\<^bsub>\<Omega>\<^esub>\<rangle>\<amalg>\<langle>\<t> \<circ>\<^sub>c \<beta>\<^bsub>\<Omega>\<^esub>,id \<Omega>\<rangle>) OR"
- 
+  "OR = NOT \<circ>\<^sub>c NOR"
  
 lemma OR_type[type_rule]:
   "OR : (\<Omega>\<times>\<^sub>c\<Omega>) \<rightarrow> \<Omega>"
-  unfolding OR_def
+  unfolding OR_def by typecheck_cfuncs
 
+lemma OR_false_false_is_false:
+  "OR \<circ>\<^sub>c \<langle>\<f>,\<f>\<rangle> = \<f>"
+  unfolding OR_def 
+  using NOR_false_false_is_true NOT_true_is_false comp_associative2 
+  by (typecheck_cfuncs, force)
 
+lemma OR_true_left_is_true:
+  assumes "p \<in>\<^sub>c \<Omega>"
+  shows "OR \<circ>\<^sub>c \<langle>\<t>,p\<rangle> = \<t>"
+  unfolding OR_def 
+  using assms NOR_left_true_is_false NOT_false_is_true comp_associative2
+  by (typecheck_cfuncs, force)
 
+lemma OR_true_right_is_true:
+  assumes "p \<in>\<^sub>c \<Omega>"
+  shows "OR \<circ>\<^sub>c \<langle>p,\<t>\<rangle> = \<t>"
+  unfolding OR_def 
+  using assms NOR_right_true_is_false NOT_false_is_true comp_associative2
+  by (typecheck_cfuncs, force)
 
 end
