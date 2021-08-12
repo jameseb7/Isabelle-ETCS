05c22100604f54b1aed5e605c3848f2060618004
Dustin Bryant <brydustin@gmail.com>
2021-08-11 21:44:35 -0500

    HALF of coprod_leq_product

--- Countable.thy
+++ Countable.thy
@@ -37,6 +37,107 @@ lemma smaller_than_countable_is_countable:
   by (smt assms cfunc_type_def comp_type composition_of_monic_pair_is_monic countable_def is_smaller_than_def)
 
 
+lemma coprod_leq_product:
+  assumes X_not_init: "\<not>(initial_object(X))" 
+  assumes Y_not_init: "\<not>(initial_object(Y))" 
+  assumes X_not_term: "\<not>(terminal_object(X))"
+  assumes Y_not_term: "\<not>(terminal_object(Y))"
+  shows "(X \<Coprod> Y) \<le>\<^sub>c (X \<times>\<^sub>c Y)"
+proof - 
+  obtain x1 x2 where x1x2_def[type_rule]:  "(x1 \<in>\<^sub>c X)" "(x2 \<in>\<^sub>c X)" "(x1 \<noteq> x2)"
+    using X_not_init X_not_term iso_empty_initial iso_to1_is_term no_el_iff_iso_0 nonempty_def single_elem_iso_one by blast
+  obtain y1 y2 where y1y2_def[type_rule]:  "(y1 \<in>\<^sub>c Y)" "(y2 \<in>\<^sub>c Y)" "(y1 \<noteq> y2)"
+    using Y_not_init Y_not_term iso_empty_initial iso_to1_is_term no_el_iff_iso_0 nonempty_def single_elem_iso_one by blast
+  then have y1_mono[type_rule]: "monomorphism(y1)"
+    using element_monomorphism by blast
+  obtain m where m_def: "m = \<langle>id(X), y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub>\<rangle> \<amalg> ((\<langle>x2, y2\<rangle> \<amalg> \<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle>) \<circ>\<^sub>c  try_cast y1)"
+    by simp
+  have type1: "\<langle>id(X), y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub>\<rangle> : X \<rightarrow> (X \<times>\<^sub>c Y)"
+    by (meson cfunc_prod_type comp_type id_type terminal_func_type y1y2_def)
+  have trycast_y1_type: "try_cast y1 : Y \<rightarrow> one \<Coprod> (Y \<setminus> (one,y1))"
+    by (meson element_monomorphism try_cast_type y1y2_def)
+  have y1'_type[type_rule]: "y1\<^sup>c : Y \<setminus> (one,y1) \<rightarrow> Y"
+    using complement_morphism_type one_terminal_object terminal_el__monomorphism y1y2_def by blast
+  have type4: "\<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle> : Y \<setminus> (one,y1) \<rightarrow> (X \<times>\<^sub>c Y)"
+    using cfunc_prod_type comp_type terminal_func_type x1x2_def y1'_type by blast
+  have type5: "\<langle>x2, y2\<rangle> \<in>\<^sub>c (X \<times>\<^sub>c Y)"
+    by (simp add: cfunc_prod_type x1x2_def y1y2_def)
+  then have type6: "\<langle>x2, y2\<rangle> \<amalg> \<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle> :(one \<Coprod> (Y \<setminus> (one,y1))) \<rightarrow> (X \<times>\<^sub>c Y)"
+    using cfunc_coprod_type type4 by blast
+  then have type7: "((\<langle>x2, y2\<rangle> \<amalg> \<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle>) \<circ>\<^sub>c  try_cast y1) : Y \<rightarrow> (X \<times>\<^sub>c Y)"
+    using comp_type trycast_y1_type by blast
+  then have m_type: "m : X  \<Coprod> Y \<rightarrow> (X \<times>\<^sub>c Y)"
+    by (simp add: cfunc_coprod_type m_def type1)
+  have "injective(m)"
+  proof(unfold injective_def ,auto)
+    fix a b 
+    assume "a \<in>\<^sub>c domain m" "b \<in>\<^sub>c domain m"
+    then have a_type[type_rule]: "a \<in>\<^sub>c X  \<Coprod> Y" and b_type[type_rule]: "b \<in>\<^sub>c X  \<Coprod> Y"
+      using m_type unfolding cfunc_type_def by auto
+    assume eqs: "m \<circ>\<^sub>c a = m \<circ>\<^sub>c b"
+
+      have m_leftproj_l_equals: "\<And> l. l  \<in>\<^sub>c X \<Longrightarrow> m \<circ>\<^sub>c left_coproj X Y \<circ>\<^sub>c l = \<langle>l, y1\<rangle>"
+      proof-
+        fix l 
+        assume l_type: "l \<in>\<^sub>c X"
+        have "m \<circ>\<^sub>c left_coproj X Y \<circ>\<^sub>c l = (\<langle>id(X), y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub>\<rangle> \<amalg> ((\<langle>x2, y2\<rangle> \<amalg> \<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle>) \<circ>\<^sub>c  try_cast y1)) \<circ>\<^sub>c left_coproj X Y \<circ>\<^sub>c l"
+          by (simp add: m_def)
+        also have "... = (\<langle>id(X), y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub>\<rangle> \<amalg> ((\<langle>x2, y2\<rangle> \<amalg> \<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle>) \<circ>\<^sub>c  try_cast y1) \<circ>\<^sub>c left_coproj X Y) \<circ>\<^sub>c l"
+          using comp_associative2 l_type by (typecheck_cfuncs, blast)
+        also have "... = \<langle>id(X), y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub>\<rangle> \<circ>\<^sub>c l"
+          by (typecheck_cfuncs, simp add: left_coproj_cfunc_coprod)
+        also have "... = \<langle>id(X)\<circ>\<^sub>c l , (y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub>) \<circ>\<^sub>c l\<rangle>"
+          using l_type cfunc_prod_comp by (typecheck_cfuncs, auto)
+        also have "... = \<langle>l , y1 \<circ>\<^sub>c \<beta>\<^bsub>X\<^esub> \<circ>\<^sub>c l\<rangle>"
+          using l_type comp_associative2 id_left_unit2 by (typecheck_cfuncs, auto)
+        also have "... = \<langle>l , y1\<rangle>"
+          using l_type by (typecheck_cfuncs,metis id_right_unit2 id_type one_unique_element)
+        then show "m \<circ>\<^sub>c left_coproj X Y \<circ>\<^sub>c l = \<langle>l,y1\<rangle>"
+          by (simp add: calculation)
+      qed
+
+      
+    show "a = b"
+    proof(cases "\<exists>x. a = left_coproj X Y \<circ>\<^sub>c x  \<and> x \<in>\<^sub>c X")
+      assume "\<exists>x. a = left_coproj X Y \<circ>\<^sub>c x  \<and> x \<in>\<^sub>c X"
+      then obtain x where x_def: "a = left_coproj X Y \<circ>\<^sub>c x  \<and> x \<in>\<^sub>c X"
+        by auto
+
+
+      then have m_proj_a: "m \<circ>\<^sub>c left_coproj X Y \<circ>\<^sub>c x = \<langle>x, y1\<rangle>"
+        using m_leftproj_l_equals by (simp add: x_def)
+      show "a = b"
+      proof(cases "\<exists>c. b = left_coproj X Y \<circ>\<^sub>c c  \<and> c \<in>\<^sub>c X")
+        assume "\<exists>c. b = left_coproj X Y \<circ>\<^sub>c c \<and> c \<in>\<^sub>c X"
+        then obtain c where c_def: "b = left_coproj X Y \<circ>\<^sub>c c  \<and> c \<in>\<^sub>c X"
+          by auto
+        then have "m \<circ>\<^sub>c left_coproj X Y \<circ>\<^sub>c c = \<langle>c, y1\<rangle>"
+          by (simp add: m_leftproj_l_equals)
+        then show ?thesis
+          using c_def element_pair_eq eqs m_proj_a x_def y1y2_def(1) by auto
+      next
+        assume "\<nexists>c. b = left_coproj X Y \<circ>\<^sub>c c \<and> c \<in>\<^sub>c X"
+        then obtain c where c_def: "b = right_coproj X Y \<circ>\<^sub>c c  \<and> c \<in>\<^sub>c Y"
+          using b_type coprojs_jointly_surj by blast
+
+        have m_rightproj_l_equals: "m \<circ>\<^sub>c right_coproj X Y \<circ>\<^sub>c c = c"
+        proof - 
+          have "m \<circ>\<^sub>c right_coproj X Y \<circ>\<^sub>c c = (m \<circ>\<^sub>c right_coproj X Y) \<circ>\<^sub>c c"
+            using c_def comp_associative2 m_type by (typecheck_cfuncs, auto)
+          also have "... = ((\<langle>x2, y2\<rangle> \<amalg> \<langle>x1 \<circ>\<^sub>c \<beta>\<^bsub>Y \<setminus> (one,y1)\<^esub>, y1\<^sup>c\<rangle>) \<circ>\<^sub>c  try_cast y1) \<circ>\<^sub>c c"
+            using m_def right_coproj_cfunc_coprod type1 by (typecheck_cfuncs, auto)
+        oops
+
+
+
+
+
+
+
+
+
+
+
 lemma product_of_finite_is_finite:
   assumes "is_finite(X)" "is_finite(Y)"
   assumes "nonempty(X)" "nonempty(Y)"
@@ -57,7 +158,7 @@ proof-
 
 lemma coproduct_of_finite_is_finite:
   assumes "is_finite(X)" "is_finite(Y)"
-  shows "is_finite(X \<Coprod>  Y)"
+  shows "is_finite(X \<Coprod> Y)"
   unfolding is_finite_def
   oops
 
@@ -73,6 +174,10 @@ proof -
   have f_type[type_rule]: "f : \<nat>\<^sub>c \<times>\<^sub>c \<nat>\<^sub>c \<rightarrow> \<nat>\<^sub>c \<times>\<^sub>c \<nat>\<^sub>c"
     unfolding f_def by typecheck_cfuncs
   oops
+
+
+
+
 
 
 (*Once we have this  result above we can generalize it to any countable sets*)
@@ -105,9 +210,20 @@ qed
 
 
 lemma NuN_is_countable:
-  "countable(\<nat>\<^sub>c \<Coprod> \<nat>\<^sub>c)"
-  unfolding countable_def
-  oops
+  assumes NxN_is_countable: "countable(\<nat>\<^sub>c \<times>\<^sub>c \<nat>\<^sub>c)"
+  assumes less_than: "(\<nat>\<^sub>c \<Coprod> \<nat>\<^sub>c) \<le>\<^sub>c (\<nat>\<^sub>c \<times>\<^sub>c \<nat>\<^sub>c)"
+  shows "countable(\<nat>\<^sub>c \<Coprod> \<nat>\<^sub>c)"
+proof -
+  have not_initial: "\<not>(initial_object(\<nat>\<^sub>c))"
+    using initial_iso_empty no_el_iff_iso_0 nonempty_def zero_type by blast
+  have not_terminal: "\<not>(terminal_object(\<nat>\<^sub>c))"
+    using epi_is_surj id_isomorphism id_type is_infinite_def iso_imp_epi_and_monic natural_numbers_are_countably_infinite terminal_object_def by blast
+  show ?thesis
+    using NxN_is_countable less_than smaller_than_countable_is_countable by blast
+qed
+
+
+
 
 
 (*Exercise 2.6.11*)
