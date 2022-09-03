64abaefdb27e9538b6d96f691cf2c794aabf3525
Dusty <dusty@pop-os.localdomain>
2022-09-03 17:19:21 -0500

    Setup

--- ETCS_Wellordering.thy
+++ ETCS_Wellordering.thy
@@ -4,14 +4,18 @@ begin
 
 theorem well_ordering_principle:
   assumes "nonempty A" "(A, m) \<subseteq>\<^sub>c \<nat>\<^sub>c"
-  shows "\<exists> a. a \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<and> (\<forall> b. b \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<longrightarrow> a \<le>\<^sub>\<nat> b)"
-proof -
+  shows "\<exists> a. a \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<and> (\<forall> b. b \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<longrightarrow> (leq \<circ>\<^sub>c \<langle>a,b\<rangle> = \<t>))" (* a \<le>\<^sub>\<nat> b*)
+proof(cases "zero \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m)")
+  show "zero \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<Longrightarrow> \<exists>a. a \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<and> (\<forall>b. b \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m) \<longrightarrow> leq \<circ>\<^sub>c \<langle>a,b\<rangle> = \<t>)"
+    using relative_member_def zero_is_smallest by blast
+next
+  assume no_zero: " \<not> zero \<in>\<^bsub>\<nat>\<^sub>c\<^esub> (A, m)"
   obtain \<chi>\<^sub>A where \<chi>\<^sub>A_def: "\<chi>\<^sub>A = characteristic_func m"
     by simp
   have \<chi>\<^sub>A_type[type_rule]: "\<chi>\<^sub>A : \<nat>\<^sub>c \<rightarrow> \<Omega>"
     using assms unfolding \<chi>\<^sub>A_def subobject_of_def2 by (auto, typecheck_cfuncs)
 
-  obtain q where q_def: "q = (\<t> \<bowtie>\<^sub>f zero) \<circ>\<^sub>c case_bool \<circ>\<^sub>c \<chi>\<^sub>A \<circ>\<^sub>c zero"
+  obtain q where q_def: "q = (right_coproj \<Omega> \<nat>\<^sub>c) \<circ>\<^sub>c zero"    (*I redefined q because we only need the special case when 0 is not an element of the set!*)
     by simp
   have q_type[type_rule]: "q : one \<rightarrow> \<Omega> \<Coprod> \<nat>\<^sub>c"
     unfolding q_def by typecheck_cfuncs
@@ -28,11 +32,13 @@ proof -
             and  u_successor: "f \<circ>\<^sub>c u = u \<circ>\<^sub>c successor"
     using natural_number_object_property2 by (typecheck_cfuncs, blast)
 
-  obtain q' where q'_def: "q' = \<langle>\<chi>\<^sub>A \<circ>\<^sub>c zero, zero\<rangle>"
+  obtain q' where q'_def: "q' = \<langle>\<f>, zero\<rangle>"   (*I redefined q' because we only need the special case when 0 is not an element of the set!*)
     by simp
   have q'_type[type_rule]: "q' : one \<rightarrow> \<Omega> \<times>\<^sub>c \<nat>\<^sub>c"
     unfolding q'_def by typecheck_cfuncs
 
+
+  
   obtain f' where f'_def: "f' = \<langle>
         AND \<circ>\<^sub>c \<langle>left_cart_proj \<Omega> \<nat>\<^sub>c, \<chi>\<^sub>A \<circ>\<^sub>c successor \<circ>\<^sub>c right_cart_proj \<Omega> \<nat>\<^sub>c\<rangle>, 
         successor \<circ>\<^sub>c right_cart_proj \<Omega> \<nat>\<^sub>c
@@ -45,6 +51,13 @@ proof -
     by simp
   have v1_type[type_rule]: "v1 : \<nat>\<^sub>c \<rightarrow> \<Omega> \<times>\<^sub>c \<nat>\<^sub>c"
     unfolding v1_def by typecheck_cfuncs
+
+   
+  have v10_eqs_q': "v1 \<circ>\<^sub>c zero = q'"
+  proof - 
+    have "FORALL \<nat>\<^sub>c \<circ>\<^sub>c (IMPLIES \<circ>\<^sub>c \<langle>leq, NOT \<circ>\<^sub>c \<chi>\<^sub>A \<circ>\<^sub>c right_cart_proj \<nat>\<^sub>c \<nat>\<^sub>c\<rangle>)\<^sup>\<sharp> \<circ>\<^sub>c zero = \<f>"
+      apply typecheck_cfuncs
+
 
   obtain v2 where v2_def: "v2 = \<langle>eq_pred (\<Omega> \<Coprod> \<nat>\<^sub>c) \<circ>\<^sub>c \<langle>u, right_coproj \<Omega> \<nat>\<^sub>c\<rangle>, id \<nat>\<^sub>c \<rangle>"
     by simp
@@ -83,5 +96,5 @@ proof -
           \<equiv>
         \<langle>eq_pred (\<Omega> \<Coprod> \<nat>\<^sub>c) \<circ>\<^sub>c \<langle>u,right_coproj \<Omega> \<nat>\<^sub>c\<rangle>,id\<^sub>c \<nat>\<^sub>c\<rangle> = \<t> \<circ>\<^sub>c \<beta>\<^bsub>\<nat>\<^sub>c\<^esub>"
       unfolding v1_def v2_def by auto
-
+    
 end
