1955d0f01016d956f80c38ba674991f0c1584e6b
James Baxter <jebaxter@hotmail.co.uk>
2026-01-03 21:36:50 +0000

    Delete some duplicate files

--- ETCS_Axioms.thy
+++
@@ -1,6 +0,0 @@
-theory ETCS_Axioms
-  imports ETCS_Base ETCS_Cartesian ETCS_Terminal ETCS_Equalizer ETCS_Truth
-    ETCS_Equivalence ETCS_Coproduct ETCS_Empty ETCS_Func ETCS_Nat ETCS_Choice
-begin
-
-end
